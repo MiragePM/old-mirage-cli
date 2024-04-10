03caebfc79d1bfdb13c2c920530b61d6eb473a8c
ZueffC <zueffc@gmail.com>
2024-04-10 17:03:48 +0300

    Fixed error

--- internal/parsers/globals.go
+++ internal/parsers/globals.go
@@ -4,7 +4,7 @@ import "os"
 
 var (
 	_homePath, _  = os.UserHomeDir()
-	_pathToConfig = _homePath + "/.config/mirage/nodes.toml"
+	_pathToConfig = _homePath + "/.config/quanta/nodes.toml"
 )
 
 func ParseHomePath() string {
